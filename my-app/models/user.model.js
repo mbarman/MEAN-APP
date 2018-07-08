3a88e0150ed8bf77934093e68e7e0333890e6e5c
mrityunjay <mrityunjay101@gmail.com>
2018-07-08 09:56:55 +0530

    code updated

---
+++ my-app/models/user.model.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var itemSchema = new mongoose.Schema({
+    name: String,
+    type: String,
+    description: String,
+    dateAdded: Date
+});
+
+itemSchema.plugin(mongoose_paginate);
+
+const Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
+
+//OR  module.exports = mongoose.model('Item', itemSchema);
